a6c1bb366ebaa74d0b0890b274cbb8d9e5d37160
DickyAulia77 <113884029+DickyAulia77@users.noreply.github.com>
2022-10-20 22:24:33 +0800

    Add files via upload

---
+++ posttest_Web/Script.js
@@ -0,0 +1,12 @@
+const Contact = document.getElementById('Contact');
+
+Contact.addEventListener("click",
+    function(){
+        alert("Belum bisa Kontol gak ush di paksa.");
+    },
+)
+ var tombol = document.getElementById("tombol");
+
+tombol.onclick = function(){
+ document.body.classList.toggle("dark-mode");
+}
